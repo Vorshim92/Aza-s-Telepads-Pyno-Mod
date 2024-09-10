ef9e58eeba703f77e1a09c1a012205b398005dc3
Stefano Scalfari <vorshim92@gmail.com>
2024-09-10 21:47:07 +0200

    test server tp

--- Contents/mods/VorshimTp/media/lua/server/VorshimTpServer.lua
+++ Contents/mods/VorshimTp/media/lua/server/VorshimTpServer.lua
@@ -3,22 +3,25 @@ local Commands = {}
 Commands.VorshimTp = {}
 
 function Commands.VorshimTp.teleportPlayer(player, args)
-    player:setX(args.x)
-    player:setY(args.y)
-    player:setZ(args.z)
-    player:setLx(args.x)
-    player:setLy(args.y)
-    player:setLz(args.z)
+    local players = getOnlinePlayers()
+    for i = 1, players:size() do
+        local p = players.get(i - 1)
+            if p == player then
+    p:setX(args.x)
+    p:setY(args.y)
+    p:setZ(args.z)
+    p:setLx(args.x)
+    p:setLy(args.y)
+    p:setLz(args.z)
+            end
+        end
 end
 
 
 
 
 local function onClientCommand(module, command, player, args)
-	if Commands[module] and Commands[module][command] then
-		local argStr = ''
-		for k,v in pairs(args) do argStr = argStr..' '..k..'='..tostring(v) end
-		-- noise('received '..module..' '..command..' '..tostring(player)..argStr)
+	if module == 'VorshimTp' and Commands[module][command] then
 		Commands[module][command](player, args)
 	end
 end
